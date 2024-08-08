149582d67eec0e1c0c835b814721e8491cb54496
Jaeyoung Lim <jalim@ethz.ch>
2024-08-08 10:18:09 +0200

    Update src/modules/fw_att_control/FixedwingAttitudeControl.cpp
    Co-authored-by: Silvan Fuhrer <silvan@auterion.com>

--- src/modules/fw_att_control/FixedwingAttitudeControl.cpp
+++ src/modules/fw_att_control/FixedwingAttitudeControl.cpp
@@ -326,7 +326,7 @@ void FixedwingAttitudeControl::Run()
 			if (_vcontrol_mode.flag_control_attitude_enabled && _in_fw_or_transition_wo_tailsitter_transition) {
 				const Eulerf setpoint(Quatf(_att_sp.q_d));
 				const float roll_body = setpoint.phi();
-				float pitch_body = setpoint.theta();
+				const float pitch_body = setpoint.theta();
 
 				if (PX4_ISFINITE(roll_body) && PX4_ISFINITE(pitch_body)) {
 
